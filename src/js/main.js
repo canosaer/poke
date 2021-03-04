0af05c7cc8d35cd0c946a6b7b383a26bac9cacb1
canosaer <canosaer@gmail.com>
2021-03-04 14:21:29 -0500

    menu transitions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
+const toggleMenuElements = {
+    toggle: document.querySelector(`.toggle`),
+    siteNavigation: document.querySelector(`.site-navigation`),
+    siteNavigationList: document.querySelector(`.site-navigation__list`),
+    socialMedia: document.querySelector(`.social-media`),
+    toggleLines: document.querySelectorAll(`.toggle__line`)
+}
+
+const returnArrow = document.querySelector(`.return-arrow`)
+
+
+
 Textblock([{
      target: ".intro__title",
      minWidth: 344,
@@ -10,22 +22,45 @@ Textblock([{
      maxWidth_VariableGrade: 300,
      container: "self",
      fontSize_Units: "rem"
-    }]);
+}]);
 
-// const toggleButton = document.querySelector(`.toggle`)
-// const siteNavigation = document.querySelector(`.site-navigation`)
-// const siteNavigationList = document.querySelector(`.site-navigation__list`)
-// const socialMedia = document.querySelector(`.social-media`)
-const toggleMenuElements = {
-    toggle: document.querySelector(`.toggle`),
-    siteNavigation: document.querySelector(`.site-navigation`),
-    siteNavigationList: document.querySelector(`.site-navigation__list`),
-    socialMedia: document.querySelector(`.social-media`),
-    toggleLines: document.querySelectorAll(`.toggle__line`)
-}
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".about__postcard", {
+    scrollTrigger: {
+        trigger: ".about__image",
+        start: "top center",
+    },
+    y: 200,
+    duration: .5
+  });
+
+gsap.from(".catering__card", {
+    scrollTrigger: {
+        trigger: ".catering",
+        start: "center bottom",
+    },
+    x: -2000,
+    duration: .6
+});
+
+gsap.to(returnArrow, {
+    scrollTrigger: {
+        trigger: ".intro__title",
+        onEnterBack: () => returnArrow.style.opacity = `0`
+    },
+});
+
+gsap.to(returnArrow, {
+    scrollTrigger: {
+        trigger: ".about",
+        start: "bottom center",
+        onEnter: () => returnArrow.style.opacity = `1`
+    },
+});
 
 class MenuToggle{
-    toggleMenuElements = null
+    elements = null
     
     constructor(elements){
         this.toggle = elements.toggle
@@ -50,4 +85,25 @@ class MenuToggle{
     }
 }
 
+class ScrollToTop{
+    arrow = null
+    
+    constructor(arrow){
+        this.arrow = arrow
+        this.setup()
+    }
+
+    setup() {
+    
+        this.clickHandler = (evt) => {
+            this.arrow.style.opacity = `0`
+            window.scrollTo(0,0)
+        }
+
+        this.arrow.addEventListener(`click`, this.clickHandler)
+    }
+}
+
 const toggleMenu = new MenuToggle(toggleMenuElements)
+
+const topScroll = new ScrollToTop(returnArrow)
